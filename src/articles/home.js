e621d9b927f444a99be243674342ebe6f5a8f024
petrushya <108490184+petrushya@users.noreply.github.com>
2024-07-05 16:48:30 +0300

    change src for img in home.js updates dist

--- src/articles/home.js
+++ src/articles/home.js
@@ -1,7 +1,7 @@
 export const homeArticle = [
   {
     "name": "Chef's Morning Smile",
-    "img": [ "", "300", "220" ],
+    "img": [ "https://raw.githubusercontent.com/petrushya/resto-page/6dcea65e00882cc9a33ee1c85d1e2b34ef6596d5/images/simpsmile.svg", "300", "220" ],
     "itemName": "Breakfast menu:",
     "info": [
       "Salad from the first ray of sun.",
