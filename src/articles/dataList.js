28b170a2067b5c4c3840e9343f4c9fd3bf0f9614
petrushya <108490184+petrushya@users.noreply.github.com>
2025-09-04 17:24:49 +0300

    adding one resource file

---
+++ src/articles/dataList.js
@@ -0,0 +1,148 @@
+const content = [
+  [1, 2, 3],
+  {
+    id: "home",
+    title: "our new offer today",
+    para: "New masterpieces from our chef.",
+    list: [4, 5, 6]
+  },
+  {
+    id: "dishes",
+    title: "exclusive dishes",
+    para: "Unrivaled creations from our chef.",
+    list: [7, 8, 9, 10, 11, 12]
+  },
+  {
+    id: "contacts",
+    title: "tiny island of mirages",
+    para: "For more information use our details.",
+    list: [13, 14]
+  },
+  {
+    name: "Chef's Morning Smile",
+    img: [ "smile", 300, 220 ],
+    itemName: "Breakfast menu:",
+    info: [
+      "Salad from the first ray of sun.",
+      "Rosy croissant with night dreams.",
+      "Refreshing drink from the morning dew."
+    ],
+    price: "Your Easy Smile!"
+  },
+  {
+    name: "Chef's Cheerful Smile",
+    img: [ "", 300, 220 ],
+    itemName: "Lunch menu:",
+    info: [
+      "Excepturi dolores delectus repudiandae reprehenderit repellendus.",
+      "Distinctio suscipit praesentium obcaecati.",
+      "Officia maxime praesentium rem qui velit.",
+      "Fuga amet iure molestias minus.",
+    ],
+    price: "Your Joyful Smile!"
+  },
+  {
+    name: "Chef's Wide Smile",
+    img: [ "", 300, 220 ],
+    itemName: "Supper menu:",
+    info: [
+      "Magnam dignissimos eos et aut.",
+      "Maxime quam dicta nihil ea doloremque est voluptatum.",
+      "Earum deserunt repellat labore.",
+      "Vitae impedit sint odit.",
+    ],
+    price: "Your Beautiful Smile!"
+  },
+  {
+    name: "Wonderful Salad with Fantasies",
+    img: [ "", 300, 220 ],
+    itemName: "Ingredients:",
+    info: [
+       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+       "Ullam deleniti quos modi iusto aliquam perspiciatis eius.",
+       "Tenetur, vero rem odit voluptates modi consequuntu."
+    ],
+    price: "Your Mysterious Smile!"
+  },
+  {
+    name: "Fantastic Dreamer's Salad",
+    img: [ "", 300, 220 ],
+    itemName: "Ingredients:",
+    info: [
+       "Maxime quam dicta nihil ea doloremque est voluptatum.",
+       "Excepturi dolores delectus repudiandae reprehenderit repellendus.",
+       "Officia maxime praesentium rem qui velit."
+    ],
+    price: "Your Dreamy Smile!"
+  },
+  {
+    name: "Chili Sunny Mix",
+    img: [ "", 300, 220 ],
+    itemName: "Ingredients:",
+    info: [
+       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+       "Ullam deleniti quos modi iusto aliquam perspiciatis eius.",
+       "Tenetur, vero rem odit voluptates modi consequuntu.",
+       "Neque debitis iste ad explicabo qui enim magnam adipisci tempore eveniet."
+    ],
+    price: "Your Shining Smile!"
+  },
+  {
+    name: "Festive Roll with Tart Sea Wind",
+    img: [ "", 300, 220 ],
+    itemName: "Ingredients:",
+    info: [
+       "Maxime quam dicta nihil ea doloremque est voluptatum.",
+       "Excepturi dolores delectus repudiandae reprehenderit repellendus.",
+       "Officia maxime praesentium rem qui velit.",
+       "Ratione ut ipsam odit velit numquam voluptatum iure voluptas rem quasi."
+    ],
+    price: "Your Friendly Smile!"
+  },
+  {
+    name: "Marine Breeze Drink",
+    img: [ "", 300, 220 ],
+    itemName: "Ingredients:",
+    info: [
+       "Maiores mollitia ipsum tempora labore assumenda sequi magni tenetur iusto.",
+       "Ipsam unde nisi.",
+       "Nobis repellendus.",
+    ],
+    price: "Your Slight Smile!"
+  },
+  {
+    name: "Evening Chill Wine",
+    img: [ "", 300, 220 ],
+    itemName: "Ingredients:",
+    info: [
+       "Ratione ut ipsam odit velit numquam voluptatum iure voluptas rem quasi.",
+       "Dolorum fuga.",
+       "Accusantium omnis."
+    ],
+    price: "Your Trusting Smile!"
+  },
+  {
+    name: "Our command",
+    img: [ "", 400, 250 ],
+    itemName: "Information:",
+    info: [
+       "XXX-XXXX-XXX-XXX, YYY-YYYY-YYY-YYY",
+       "mirage@magicmail.wiz",
+       "@mirage&miracle"
+    ],
+    price: null
+  },
+  {
+    name: "Chef",
+    img: [ "", 200, 250 ],
+    itemName: "Strange Wizard",
+    info: [
+       "XXX-XXXX-XXX-XXX",
+       "weird@magicmail.wiz",
+       "@freakfigure"
+    ],
+    price: null
+  }
+];
+
+export default content;
